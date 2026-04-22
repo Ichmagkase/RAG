0414d576c23ec60576760d0a3018fdc8719a44da
Zack <ichmagkase70@gmail.com>
2026-04-22 18:57:16 -0400

    task: added repl to main

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
+def add_context(user_input):
+    return user_input
+
+
+def prompt(query):
+    return query
+
+
+def repl():
+    while True:
+        user_input = input("> ")
+        if user_input == "q":
+            break
+        query = add_context(user_input)
+        prompt(query)
+
+
 def main():
-    print("Hello from rag!")
+    repl()
 
 
 if __name__ == "__main__":
