2a3570f3ff2ab7a6ef4f67375c69d6ba1bb00754
Zack <ichmagkase70@gmail.com>
2026-04-22 19:32:43 -0400

    task: refactored transformer class

--- transformer.py
+++ transformer.py
@@ -1,19 +1,21 @@
 from sentence_transformers import SentenceTransformer
 
-# 1. Load a pretrained Sentence Transformer model
-model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
 
-# The sentences to encode
-sentences = [
-    "The weather is lovely today.",
-    "It's so sunny outside!",
-    "He drove to the stadium.",
-]
+class Transformer:
+    def __init__(self):
+        # 1. Load a pretrained Sentence Transformer model
+        self.model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
 
-# 2. Calculate embeddings by calling model.encode()
-embeddings = model.encode(sentences)
-print(embeddings.shape)
+    def compare(self, sentence1, sentence2):
+        embeddings = self.model.encode([sentence1, sentence2])
+        similarities = self.model.similarity(embeddings, embeddings)
+        difference = similarities[0][0] - similarities[0][1]
 
-# 3. Calculate the embedding similarities
-similarities = model.similarity(embeddings, embeddings)
-print(similarities)
+        return difference
+
+
+if __name__ == "__main__":
+    # Example usage:
+    t = Transformer()
+    print(t.compare("I walked my dog yesterday", "I own a cat"))
+    print(t.compare("I walked my dog today", "dogs like to eat dog food"))
