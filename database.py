9cce51233b56ebfcbc36a8e4c6023e33224cf0d4
Zack <ichmagkase70@gmail.com>
2026-04-22 20:28:01 -0400

    task: implemented store_memory()

--- database.py
+++ database.py
@@ -1,5 +1,6 @@
 import sqlite3
 from memory import Memory
+import pickle
 
 
 class DBConnection:
@@ -18,6 +19,17 @@ class DBConnection:
         """)
 
     def store_memory(self, memory: Memory):
-        self.cur.execute("""
-            
-        """)
+        # 1. Serialize the embedding vector into bytes (BLOB)
+        embedding_blob = pickle.dumps(memory.embedding)
+
+        # 2. Execute the INSERT statement using parameterized queries
+        self.cur.execute(
+            """
+            INSERT INTO memories (content, embedding, importance)
+            VALUES (?, ?, ?)
+        """,
+            (memory.content, embedding_blob, memory.importance),
+        )
+
+        # 3. Commit the transaction to save the changes to the file
+        self.con.commit()
