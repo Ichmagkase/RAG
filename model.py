c8185eb286349b2c105066f836d70ad99c41e61d
jpwol <jpwolfe1981@gmail.com>
2026-04-23 18:48:57 -0400

    fix: formatted prompts for llama specifically

--- model.py
+++ model.py
@@ -11,6 +11,17 @@ class Model:
         FastLanguageModel.for_inference(self.model)
 
     def prompt(self, query: str) -> str:
-        inputs = self.tokenizer(query, return_tensors="pt").to("cuda")
-        outputs = self.model.generate(**inputs, max_new_tokens=256)
-        return self.tokenizer.decode(outputs[0], skip_special_tokens=True)
+        messages = [{"role": "user", "content": query}]
+        # inputs = self.tokenizer(query, return_tensors="pt").to("cuda")
+        inputs = self.tokenizer.apply_chat_template(
+            messages,
+            tokenize=True,
+            add_generation_prompt=True,
+            return_tensors="pt",
+            return_dict=True,
+        ).to("cuda")
+        input_length = inputs["input_ids"].shape[1]
+        outputs = self.model.generate(**inputs, max_new_tokens=256, max_length=None)
+        return self.tokenizer.decode(
+            outputs[0][input_length:], skip_special_tokens=True
+        )
